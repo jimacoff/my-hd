c5cb21ece283507cc912850662dcd07b28954fed
Sidney <leather.s.dev@gmail.com>
2018-04-22 12:43:47 -0700

    added major styling for videso#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'videos#show'
 
   get 'privacy', to: 'pages#privacy'
   get 'terms', to: 'pages#terms'
+
+  resources :videos, only: [:index, :show]
 end
