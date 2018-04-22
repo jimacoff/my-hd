3cde31388e1f57c727af574e106a136c45b5e74b
Sidney <leather.s.dev@gmail.com>
2018-04-21 17:18:03 -0700

    added nabber and logo

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
+![readme-logo](https://user-images.githubusercontent.com/19522903/39089995-c7d2e8d4-4587-11e8-9c45-e882c3248a4b.png)
+
 # My-HD
+
+#### An open-source production ready Ruby/Rails video publishing app with VAST pre-roll and mid-roll ad support
 
 ## Introduction
 
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,7 +9,6 @@ Rails.application.config.assets.precompile += ['node_modules/bootstrap/dist/js/*
 Rails.application.config.assets.precompile += ['node_modules/bootstrap/dist/css/*.css']
 Rails.application.config.assets.precompile += ['node_modules/jquery/dist/*.js']
 
-
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
